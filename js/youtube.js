324045a4115bcca49ae9a1ddf3954d26ca3e961b
Sroaj Sosothikul <sroaj@sosothikul.com>
2016-09-28 02:13:31 -0700

    Removed 2 seconds off SpaceX - Webcast Music 15.06.2016

--- js/youtube.js
+++ js/youtube.js
@@ -21,7 +21,7 @@ var trackList = [
     {'videoId': 'IOUjcEtT5_w', 'startSeconds':0, 'endSeconds': undefined }, //SpaceX Webcast Music 21.12.2015 Intro 1 | ORBCOMM-2 Mission
     {'videoId': 'yXAEAUN6Az8', 'startSeconds':0, 'endSeconds': undefined }, //SpaceX Webcast Music 21.12.2015 Intro 2 | ORBCOMM-2 Mission
     {'videoId': 'd0X82bh42gQ', 'startSeconds':0, 'endSeconds': undefined }, //SpaceX Webcast Music 21.12.2015 outro | ORBCOMM-2 Mission
-    {'videoId': 'zLbUTy0K97c', 'startSeconds':283, 'endSeconds': 572 }, //SpaceX - Webcast Music 15.06.2016
+    {'videoId': 'zLbUTy0K97c', 'startSeconds':283, 'endSeconds': 570 }, //SpaceX - Webcast Music 15.06.2016
 	{'videoId': 'hVTXPwNBu7g', 'startSeconds':0, 'endSeconds': undefined }, //The Strangers by The Deep
 	{'videoId': 'A1YxNYiyALg', 'startSeconds':0, 'endSeconds': 263 }, //Resonator by Test Shot Starfish
 	{'videoId': '0qo78R_yYFA', 'startSeconds':0, 'endSeconds': undefined }, //SpaceX Interplanetary Transport System
