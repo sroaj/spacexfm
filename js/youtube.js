d989fb0b74e24c19cc4baae02e4950e35913a057
Sroaj Sosothikul <sroaj@sosothikul.com>
2017-06-04 01:33:52 -0700

    Added CRS-10 music

--- js/youtube.js
+++ js/youtube.js
@@ -33,6 +33,8 @@ var trackList = [
     {'videoId': '0qo78R_yYFA', 'startSeconds':0, 'endSeconds': undefined,    'name': 'SpaceX Interplanetary Transport System', 'artist': 'Unknown' },
     {'videoId': 'HKrXcfcz6Ps', 'startSeconds':0, 'endSeconds': 268,          'name': 'Return To Flight',                       'artist': 'Test Shot Starfish' },
     {'videoId': 'HKrXcfcz6Ps', 'startSeconds':269, 'endSeconds': 566,        'name': 'Fly By',                                 'artist': 'Test Shot Starfish' },
+    {'videoId': 'qPVRRtNxw5Q', 'startSeconds':0, 'endSeconds': 250,          'name': 'LC-39A',                                 'artist': 'Test Shot Starfish' },
+    {'videoId': 'qPVRRtNxw5Q', 'startSeconds':250, 'endSeconds': 548,        'name': 'Andromeda',                              'artist': 'Test Shot Starfish' },
 ];
 
 var trackPlaying = false;
